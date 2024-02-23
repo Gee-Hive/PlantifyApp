3912e30757076d0e9758db8e45fdaae43f090d69
Ogunleye Gbenga <gbenga@getwallets.co>
2024-02-23 03:08:20 +0100

    implement checkBox, build TC and PP links

---
+++ src/constants/links.js
@@ -0,0 +1,5 @@
+export const PRIVACY_POLICY_LINK =
+  'https://www.freeprivacypolicy.com/live/a3fc9e9f-8cd1-48cc-b646-e2874fe17b1c';
+
+export const TERMS_CONDITIONS_LINK =
+  'https://www.freeprivacypolicy.com/live/c790833a-eb37-4b0b-8d4f-27fbfd6d790e';
--- src/screens/auth/Siginup/index.js
+++ src/screens/auth/Siginup/index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {Linking, SafeAreaView, Text, View} from 'react-native';
 import Button from '../../../components/Button';
 import styles from './styles';
 import Input from '../../../components/Input';
 import Title from '../../../components/Title';
+import Checkbox from '../../../components/Checkbox';
+import {
+  PRIVACY_POLICY_LINK,
+  TERMS_CONDITIONS_LINK,
+} from '../../../constants/links';
 
 const Signup = ({navigation}) => {
+  const [agreed, setAgreed] = useState(false);
+
+  const onCheckBoxPress = () => {
+    setAgreed(value => !value);
+  };
+
+  const onLinkPress = url => {
+    Linking.openURL(url);
+  };
   return (
     <SafeAreaView style={styles.container}>
       <Title>Join the Hub!</Title>
@@ -15,6 +29,28 @@ const Signup = ({navigation}) => {
       <Input placeholder="Email" />
       <Input placeholder="Password" />
       <Input placeholder="Confirm Password" />
+
+      <View style={styles.row}>
+        <Checkbox checked={agreed} onPress={onCheckBoxPress} />
+
+        <Text style={styles.agreeText}>
+          I agree to
+          <Text
+            style={styles.link}
+            onPress={() => onLinkPress(TERMS_CONDITIONS_LINK)}>
+            {' '}
+            Terms and Conditions
+          </Text>{' '}
+          and
+          <Text
+            style={styles.link}
+            onPress={() => onLinkPress(PRIVACY_POLICY_LINK)}>
+            {' '}
+            Privacy Policy
+          </Text>
+        </Text>
+      </View>
+
       <Button type="blue">Create new account</Button>
 
       <Text style={styles.footerText}>
