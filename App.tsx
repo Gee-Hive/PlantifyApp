8b21cbd3e29522b883bf7881bc71455d39240d40
Ogunleye Gbenga <gbenga@getwallets.co>
2024-03-08 19:41:50 +0100

    complete and finalize all requirements meant for auth(sign-up and sign-in)

--- App.tsx
+++ App.tsx
@@ -9,21 +9,12 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 
-const Stack = createStackNavigator();
-
-import OnBoarding from './src/screens/auth/OnBoarding';
-import {createStackNavigator} from '@react-navigation/stack';
-import Signin from './src/screens/auth/Signin';
-import Siginup from './src/screens/auth/Siginup';
+import Routes from './src/routes';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="OnBoarding" component={OnBoarding} />
-        <Stack.Screen name="Signin" component={Signin} />
-        <Stack.Screen name="Signup" component={Siginup} />
-      </Stack.Navigator>
+      <Routes />
     </NavigationContainer>
   );
 }
